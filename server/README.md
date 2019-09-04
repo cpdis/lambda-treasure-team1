2ea83f5446c6c8ca0813ae9a75f7fefcd0dc0d26
colin <colin.dismuke@gmail.com>
2019-09-04 18:14:20 +0100

    Set up app.js and got initial room data from API

---
+++ server/README.md
@@ -0,0 +1,24 @@
+<h1 align="center">Lambda Treasure Hunt Backend - Team 1 💰</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+</p>
+
+> You start the adventure unworthy, unequipped and anonymous. Your first task is to traverse the island and build a map for your personal use. Along the way, you will discover equipment, treasure and clues which will assist you on your quest for power, wealth and glory.
+
+## Install
+
+```sh
+yarn
+```
+
+## Usage
+
+```sh
+node app.js
+```
+
+## Authors
+
+👤 **Stefan Clem, William Yturralde, Lorenzo Evans, Colin Dismuke**
+
+---
---
+++ server/app.js
@@ -0,0 +1,46 @@
+const treasureHunt = require("./axios_config");
+const move = require("./graph");
+
+// Create empty arrays and list to hold map and paths
+let traversalPath = [];
+let reversePath = [];
+let map = {};
+
+// Create a variable for the current room
+let currentRoom = null;
+let coolDown = 16; // To account for the 15 second cool down period
+
+// Create a helper function to reverse the N, S, E, W direction
+const reverse = direction => {
+  let result = "";
+
+  if (direction == "n") {
+    result = "s";
+  } else if (direction == "s") {
+    result = "n";
+  } else if (direction == "w") {
+    result = "e";
+  } else if (direction == "e") {
+    result = "w";
+  }
+
+  return result;
+};
+
+// Initialize
+treasureHunt
+  .get("init")
+  .then(res => {
+    console.log("init: ", res.data);
+
+    // Set the current_room to res.data
+    currentRoom = res.data;
+
+    // Print out the current room ID and the exits
+    console.log("Room ID: ", currentRoom.room_id);
+    console.log("Room exits: ", currentRoom.exits);
+
+    // Set the cool down period to whatever it is in the current room
+    coolDown = currentRoom.cooldown;
+  })
+  .catch(err => console.error(err));
