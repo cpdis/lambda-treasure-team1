9f9ef2d8715bf8bfdc8fa9a55e0f3376a0d41285
Stefan <steaclem@gmail.com>
2019-09-05 19:21:33 -0700

    Added Axios for the front end Added react-vis for the map Added dotenv for env files And updated yarn

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
+import dotenv
+import { FlexibleXYPlot, LineSeries, MarkSeries } from "react-vis";
 
+const URL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/";
+const token = process.env.REACT_APP_API_KEY;
 export default class Map extends Component {
+  state = {
+    nodes: []
+  };
+  componentDidMount() {
+    axios.get("URL", token);
+  }
   render() {
+    const { nodes } = this.state;
+
     return <div>Treasure Map</div>;
   }
 }
