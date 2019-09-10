451be3046208964337a489e6f088e3bd684e8f36
Stefan <steaclem@gmail.com>
2019-09-09 20:48:59 -0700

    Added Code that Colin showed me. Will need a database for our real graph. This is tempoary until we get new one.

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import { CssBaseline } from "@material-ui/core";
 import "./App.css";
+import Map from "./components/Map";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Route exact path="/" component={Landing} />
       <Route path="/home" component={Home} />
       <Route path="/about" component={About} />
+      <Map />
     </div>
   );
 };
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,24 +1,62 @@
 import React, { Component } from "react";
-import axios from "axios";
-import data from "server\map_data.json";
-
 import { FlexibleXYPlot, LineSeries, MarkSeries } from "react-vis";
+import room_data from "../data/graph";
+import styled from "styled-components";
 
-const URL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/";
-const token = process.env.REACT_APP_API_KEY;
-export default class Map extends Component {
-  state = {
-    map: {},
-    mapLoaded: false
-  };
-  componentDidMount() {
-    if (!localStorage.hasOwnProperty("data")) {
-      localStorage.setItem("data", JSON.stringify(map));
-    }
-    let val = JSON.parse(localStorage.getItem("map"));
-    this.setState({ map: val, mapLoaded: true });
-  }
+const StyledMap = styled.div`
+  marginleft: 10%;
+  width: 66%;
+  height: 100%;
+  flex: 1;
+  padding: 3rem 3rem 3rem 3rem;
+  position: relative;
+`;
+
+class Map extends Component {
   render() {
-    return <div>Treasure Map</div>;
+    // Create arrays to hold point coordinates and links
+    const coordinates = [];
+    const links = [];
+
+    // Loop through each room in the room_data object
+    for (let room in room_data) {
+      // Set data equal to the first element (x, y coordinates)
+      // in each room of the room_data object
+      let data = room_data[room][0];
+      coordinates.push(data);
+      for (let adjacentRoom in room_data[room][1]) {
+        links.push([
+          room_data[room][0],
+          room_data[room_data[room][1][adjacentRoom]][0]
+        ]);
+      }
+    }
+
+    return (
+      <StyledMap>
+        <FlexibleXYPlot width={900} height={800}>
+          {links.map(link => (
+            <LineSeries
+              strokeWidth="5"
+              color="orangered"
+              data={link}
+              key={Math.random() * 100}
+            />
+          ))}
+          <MarkSeries
+            current={this.props.currentRoom}
+            highlight="none"
+            strokeWidth={5}
+            opacity="1"
+            size="5"
+            color="navy"
+            data={coordinates}
+            style={{ cursor: "pointer" }}
+          />
+        </FlexibleXYPlot>
+      </StyledMap>
+    );
   }
 }
+
+export default Map;
