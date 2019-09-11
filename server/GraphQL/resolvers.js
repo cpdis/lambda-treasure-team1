721c096dfb77ae6250ee791d9d9847fa83126029
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-10 23:38:50 -0400

    Fleshing out room(s) resolver.

--- server/GraphQL/resolvers.js
+++ server/GraphQL/resolvers.js
@@ -3,6 +3,20 @@ const rooms = './server/graph_data.json';
 const resolvers = {
   Query: {
     rooms: () => rooms
+  },
+  Room: {
+    room_id: (parent) => parent.id,
+    title: (parent) => parent.title,
+    description: (parent) => parent.description,
+    coordinates: (parent) => parent.coordinates,
+    elevation: (parent) => parent.elevation,
+    terrain: (parent) => parent.terrain,
+    players: (parent) => parent.players,
+    items: (parent) => parent.items,
+    exits: (parent) => parent.exits,
+    cooldown: (parent) => parent.cooldown,
+    errors: (parent) => parent.errors,
+    messages: (parent) => parent.messages,
   }
 };
 
