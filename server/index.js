48b89dee7fd159bd9768904b51175d9b2b73d666
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-09-10 22:24:09 -0400

    Quick tear down and re-write, soz.

--- server/index.js
+++ server/index.js
@@ -1,68 +1,9 @@
-import { buildSchema} from "graphql";
-const express = require('express');
-const GqlHTTP = require('express-graphql');
-const server = express();
+import { GraphQLServer} from "graphql-yoga";
 
-const schema = buildSchema(`
-    enum Error {
-        String
-  }
-  
-  enum Message {
-      Strings
-  }
-  
-  enum Exit {
-      String
-  }
-  
-  enum Item {
-      String
-  }
-  
-  enum Status {
-      String
-  }
-  
-  type Player {
-      name: String!
-      cooldown: Float!
-      encumbrance: Int!
-      strength: Int!
-      speed: Int!
-      gold: Int!
-      inventory: [Item!]
-      status: [Status!]
-      errors: [Error]
-      messages: [Message]
-  }
-  
-  type Room {
-      room_id: Int!
-      title: String!
-      description: String!
-      coordinates: String!
-      elevation: Int!
-      terrain: String!
-      players: [Player!]
-      items: [Item!]
-      exits: [Exit]
-      cooldown: Int!
-      errors: [Error]
-      messages: [Message]
-  }
-`);
+const server = new GraphQLServer({
 
-const root = {
-  query: () => {
-  return -1;
-  }
-};
+})
 
-const GQLServer = server.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
+server.start(_, () => {
 
-GQLServer.listen(4000);
+})
