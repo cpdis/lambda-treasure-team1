92e1aef11d71a1282c885acd0970f4d86ddcf783
Stefan <steaclem@gmail.com>
2019-09-12 20:49:48 -0700

    Added Map Added Landing and Room.js Added the Info.js Tried to minmize

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -1,7 +1,14 @@
 import React from "react";
-
-const Info = () => {
-  return <div>Room Information</div>;
+import { Room } from "./Room";
+const Info = (list, currentRoom) => {
+  console.log("This is the List of Data", Object.values(list[0][0]));
+  return (
+    <div>
+      {list.map(({ room, index }) => {
+        return <Room room={room} room_id={currentRoom} />;
+      })}
+    </div>
+  );
 };
 
 export default Info;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,26 @@
-import React from "react";
-
-const Landing = () => {
-  return <div>Landing</div>;
-};
+import React, { Component } from "react";
+import data from "../data/graph_data.json";
+import Info from "./Info.js";
+class Landing extends Component {
+  state = {
+    data: [data],
+    currentRoom: 465,
+    id: Date.now() + 1
+  };
+  roomData = () => {};
+  render() {
+    const data = this.state;
+    console.log(data);
+    return (
+      <div>``
+        <Info
+          list={this.state.data}
+          key={this.state.id}
+          currentRoom={this.state.currentRoom}
+        />
+      </div>
+    );
+  }
+}
 
 export default Landing;
