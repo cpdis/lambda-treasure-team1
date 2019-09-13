1afc555b06c874e4d5648eb988bddcb944a73d6c
Colin Dismuke <colin.dismuke@gmail.com>
2019-09-13 17:11:18 -0500

    :construction: Added getLastProof function to mine

--- server/blockchain.js
+++ server/blockchain.js
@@ -1,4 +1,4 @@
-const server = require("./axios_config_blockchain");
+const blockchain = require("./axios_config_blockchain");
 const shajs = require("sha.js");
 
 hash = str => {
@@ -20,7 +20,32 @@ valid_proof = (lastProof, proof, difficulty) => {
 
 getLastProof = () => {
   let last_proof = "";
-  console.log(last_proof);
+
+  blockchain
+    .get("last_proof/")
+    .then(res => {
+      last_proof = JSON.stringify(res.data.proof);
+      let difficulty = res.data.difficulty;
+      let proof = 8675309;
+
+      console.log("Successfully made a last_proof request!");
+
+      while (!valid_proof(last_proof, proof, difficulty)) {
+        proof = Math.floor(Math.random() * 1000000000);
+      }
+
+      console.log("Found a valid proof!");
+
+      setTimeout(() => {
+        blockchain
+          .post("mine/", { proof: proof })
+          .then(res => {
+            console.log("Succesfully mined!");
+          })
+          .catch(err => console.log("Error mining: ", err));
+      }, 10 * 1000);
+    })
+    .catch(err => console.log("Error processing the last_proof request:", err));
 };
 
 module.exports = getLastProof;
