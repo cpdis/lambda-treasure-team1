c83e99e79f85794289b0876bbdd109d196334ddc
Colin Dismuke <colin.dismuke@gmail.com>
2019-09-17 21:13:44 -0500

    :construction: Added getStatus and move functions

--- client/src/App.js
+++ client/src/App.js
@@ -112,6 +112,82 @@ class App extends Component {
       .catch(err => console.log("Error getting initial status", err));
   }
 
+  getStatus = () => {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${this.state.api_key}`
+      }
+    };
+
+    axios
+      .post(
+        `https://lambda-treasure-hunt.herokuapp.com/api/adv/status`,
+        options
+      )
+      .then(res => {
+        let playerName = res.data.name;
+        let speed = res.data.speed;
+        let strength = res.data.strength;
+        let inventory = res.data.inventory;
+        let encumbrance = res.data.encumbrance;
+        let messages = res.data.messages;
+        let gold = res.data.gold;
+
+        this.setState({
+          player: {
+            name: playerName,
+            speed: speed,
+            strength: strength,
+            inventory: inventory,
+            encumbrance: encumbrance,
+            messages: messages,
+            gold: gold
+          }
+        });
+      })
+      .catch(err => console.log("Error in the status() function", err));
+  };
+
+  move = direction => {
+    axios
+      .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move", {
+        dir: direction
+      })
+      .then(res => {
+        let room_id = res.data.room_id;
+        let exits = res.data.exits;
+        let description = res.data.description;
+        let items = res.data.items;
+        let messages = res.data.messages;
+        let terrain = res.data.terrain;
+        let title = res.data.title;
+        let elevation = res.data.elevation;
+        let coordinates = res.data.coordinates;
+        let cooldown = res.data.cooldown;
+
+        this.setState({
+          room: {
+            room_id: room_id,
+            exits: exits,
+            description: description,
+            items: items,
+            messages: messages,
+            terrain: terrain,
+            title: title,
+            elevation: elevation,
+            coordinates: coordinates,
+            cooldown: cooldown
+          }
+        });
+      })
+      .catch(err => {
+        console.log("Error while moving", err);
+      });
+
+    this.getStatus();
+  };
+
   render() {
     return (
       <div className="App">
